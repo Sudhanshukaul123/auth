99dcd30cab54cab8f5d63dfd6385a22a0de0b363
Krix <sudhanshukaul504@gmail.com>
2025-06-06 23:35:44 +0530

    commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Backend.urls'
+ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
     {
@@ -64,7 +64,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Backend.wsgi.application'
+WSGI_APPLICATION = 'backend.wsgi.application'
 
 
 # Database
